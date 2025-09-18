8cd8bda3904953c6c0d224840e6faee55facb400
Marcelo <marcelo.vecchio@softtek.com>
2025-09-17 22:47:47 -0300

    Texto en el pie de página

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 VERSION = "1.0.32"
 TITULO_APP = f"🧢 SISTEMA CAPPELLO vs {VERSION}"
-FOOTER_APP = "Sistema Capello® - Brought to you by Bairesman® - 2025"
+FOOTER_APP = "Sistema Capello® - Powered by Python - Brought to you by Bairesman® - 2025"
 
 RUBRO_DEFAULT = "GORRAS"
 VENDEDOR_DEFAULT = "VICENTE"
